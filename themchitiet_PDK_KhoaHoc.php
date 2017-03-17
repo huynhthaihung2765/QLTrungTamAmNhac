538a96bb32efd85b21723d8a0476a279836aaacc
HungHuynh <huynhthaihung2765@gmail.com>
2017-03-17 13:32:08 +0700

    database mới

--- themchitiet_PDK_KhoaHoc.php
+++ themchitiet_PDK_KhoaHoc.php
@@ -71,10 +71,10 @@
                          <img style="width: 100%; display: block;" src="public/images/VatLieu/GuiTar/1.png" alt="image" />
                          <div class="mask">
                            <p>Your Text</p>
-                           <div class="tools tools-bottom">
-                             <a href="#"><i class="fa fa-link"></i></a>
-                             <a href="#"><i class="fa fa-pencil"></i></a>
-                             <a href="#"><i class="fa fa-times"></i></a>
+                           <div class="tools tools-bottom" data-toggle="modal" data-target="#myModal">
+                             
+                             <a href="#">Thêm <i class="fa fa-pencil"></i></a>
+                             
                            </div>
                          </div>
                        </div>
@@ -84,6 +84,7 @@
                        </div>
                      </div>
                    </div>
+
                    <?php if ($dem == 5){ ?>
                       <div class="clearfix"> </div>
                       <?php $dem = 0 ?>
@@ -101,5 +102,76 @@
     </div>
   </div>
 </div>
+<!--  -->
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Tên lớp <small>cấp độ</small> </h4>
+      </div>
+      <div class="modal-body">
+        <!-- form thêm chi tiết phiếu dky -->
+        <div class="col-md-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Thông tin phiếu đăng ký</h2>
 
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                    <br />
+                    <form class="form-horizontal form-label-left">
+
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Ngày bắt đầu</label>
+                        <div class="col-md-9 col-sm-9 col-xs-12">
+                          <input type="date" class="form-control" placeholder="ngày/tháng/năm">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Giáo viên</label>
+                        <div class="col-md-9 col-sm-9 col-xs-12">
+                          <select class="form-control">
+                            <option>Nguyễn Văn A</option>
+                            <option>Nguyễn Văn B</option>
+                            <option>Nguyễn Văn C</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Ngày trong tuần</label>
+                        <div class="col-md-9 col-sm-9 col-xs-12">
+                          <select class="form-control">
+                            <option>Thứ 2 / 4 / 6</option>
+                            <option>Thứ 3 / 5 / 7</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Ca trong ngày</label>
+                        <div class="col-md-9 col-sm-9 col-xs-12">
+                          <select class="form-control">
+                            <option>Ca sáng</option>
+                            <option>Ca chiều</option>
+                            <option>Ca tối</option> 
+                          </select>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+        <!-- /form thêm chi tiết phiếu dky -->
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!--  -->
 <?php include_once("footer.php") ?>
